6c7360b798d76ef50f713b5cac1f331da872020c
paiai <leesu@leesuui-MacBook-Pro.local>
2020-03-06 19:26:34 +0900

    react commit

---
+++ opentutorials/memo/memo.js
@@ -0,0 +1,28 @@
+//function type
+
+import React from 'react';
+import './App.css';
+function App() {
+    return (
+        <div className="App">
+
+        </div>
+    )
+}
+export default App;
+
+
+// Class type
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return(
+            <div className="App">
+
+            </div>
+        )
+    }
+}
+export default App;
